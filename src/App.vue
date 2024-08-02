ec4e4c58fc59fbac34a87e13fef40d75a973ee55
andreevgs <gleb.andreevv12@gmail.com>
2024-08-03 02:12:15 +0300

    implement basic push notifications

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import PWABadge from './components/PWABadge.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import PWABadge from "./components/PWABadge.vue";
 </script>
 
 <template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,59 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
+const hostname = ref<string>("");
 
-function registerBackgroundSync() {
-  if (!navigator.serviceWorker){
-    return console.error("Service Worker not supported")
-  }
-
-  navigator.serviceWorker.ready
-      .then(registration => {(registration as any).periodicSync.register('periodicSync', {minInterval: 1000}); console.log(registration)})
-      .then(() => console.log("Registered background sync"))
-      .catch(err => console.error("Error registering background sync", err))
-}
-
-function unregisterBackgroundSync() {
+function subscribeNotifications() {
   if (!navigator.serviceWorker) {
-    return console.error("Service Worker not supported");
+    return alert("Service Worker not supported");
   }
 
-  navigator.serviceWorker.ready
-      .then(registration => {
-        registration.unregister()
-          return (registration as any).periodicSync.unregister('syncAttendees');
-        })
-      .then(() => console.log("Unregistered background sync"))
-      .catch(err => console.error("Error unregistering background sync", err));
-}
-
-function registerNotification() {
-  Notification.requestPermission(notificationPermission => {
-    if (notificationPermission === 'granted'){
-      navigator.permissions.query({
-        // @ts-ignore
-        name: 'periodic-background-sync',
-      }).then(backgroundSyncPermission => {
-        console.log(backgroundSyncPermission)
-        if(backgroundSyncPermission.state === 'granted'){
-          registerBackgroundSync()
-        }
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.pushManager
+      .subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          "BJ2TBvZx9d95NVO_lBj4uekE3mhaZJAJH4JX4BG98lcM4EvPNIE8JTuNC3cVwZ-yg1m5N4E-b6m9QfUi6-NVx-c",
+        ),
+      })
+      .then((subscription) => {
+        console.log(subscription);
+        fetch(`http://${hostname.value}/subscribe`, {
+          method: "POST",
+          body: JSON.stringify(subscription),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
       });
-    }
-    else console.error("Permission was not granted.")
-  })
+  });
 }
 
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="registerNotification()">Notify</button>
-    <button type="button" @click="unregisterBackgroundSync()">Stop</button>
+    <input type="text" v-model="hostname" />
+    <button
+      type="button"
+      @click="subscribeNotifications()"
+      :disabled="!hostname"
+    >
+      Notify
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
